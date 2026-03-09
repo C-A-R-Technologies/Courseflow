a14211190e8515bf49a5a806e58a6ea1d2a9520b
avery <averwhy@gmail.com>
2026-03-09 00:20:07 -0400

    Add files for authentication

--- src/lib/env/private.ts
+++ src/lib/env/private.ts
@@ -7,16 +7,19 @@ export const privateEnv = z
         DATABASE_URL: z.url(),
         /** JWT access token secrets. */
         JWT_ACCESS_SECRET: z.string(),
+        /** JWT reset password token secrets. */
+        JWT_RESET_PASSWORD_SECRET: z.string(),
         /** Runtime environment. Currently only changes whether secure cookies are used. */
         NODE_ENV: z.enum(["development", "production", "test"]),
         /** If the specified user does not exist on startup, it will be created with the specified email and `INIT_PASSWORD`. */
         INIT_EMAIL: z.string().optional(),
-        /** If the specified user does not exist on startup, it will be created with the specified password and `NIT_USERNAME`. */
+        /** If the specified user does not exist on startup, it will be created with the specified password and `INIT_USERNAME`. */
         INIT_PASSWORD: z.string().optional(),
     })
     .transform(env => {
         const {
             JWT_ACCESS_SECRET,
+            JWT_RESET_PASSWORD_SECRET,
             INIT_EMAIL,
             INIT_PASSWORD,
             ...rest
@@ -27,6 +30,7 @@ export const privateEnv = z
             ...{
                 jwtSecrets: {
                     access: JWT_ACCESS_SECRET,
+                    resetPassword: JWT_RESET_PASSWORD_SECRET,
                 },
             },
         };
